ad0eca93f684a03e870acaa9c133d1f18ce6cabe
Hans Dietrich <hansidietrich06@gmail.com>
2025-05-07 11:08:36 -0300

    Update halftone tp_edna.py
    Ya esta terminado el halftone. Totalmente modulado con funciones separadas del codigo main. solo quedaria pasar las funciones a otro archivo

--- tp_edna.py
+++ tp_edna.py
@@ -1,7 +1,10 @@
 from PIL import Image
 import numpy as np
 import math
-
+dot_size = 5 #LO PONE EL USUARIO
+angle_r = 15
+angle_g = 45
+angle_b = 10
 def get_grid_coords(h, w, dot_size, angle_deg):
     positions = []
     angle_rad = math.radians(angle_deg)
@@ -43,74 +46,45 @@ def height_width(r,g,b): #ACA CONSEGUIMOS EL ANCHO Y ALTO DE CADA CANAL, PARA LU
     height_green , width_green= b.shape
     return height_red, width_red, height_blue, width_blue, height_green, width_green
 
+def draw_circle(chords_list:list, dot_size:float, height, width, array_white, color):
+    for chord in chords_list:
+        column = int(chord[0])
+        row = int(chord[1])
+
+        pixel_intensity = color[row,column]
+
+        radius = ((1 - pixel_intensity / 255)* dot_size *0.7)
+
+        #RECORRO UNA CUADRICULA CERCA DEL CIRCULO QUE SE FIJE QUE PUNTOS ESTAN DENTRO DEL CIRCULO
+        for i in range(int(row - radius),int(row + radius)+1):  
+            for j in range(int(column - radius),int(column + radius)+1):
+                if 0 <= i < height and 0 <= j < width: #SE FIJA SI ESTAMOS EN LA MATRIZ
+                    if (i - row)**2 + (j - column)**2 <= radius**2: #CHEQUEO ESTO DE ECUACION DADA
+                        array_white[i, j] = 0
+    return array_white
+
 #DEFINIMOS LA IMAGEN Y SEPARAMOS EN CANALES
 r, g, b = separar_rgb("/Users/hansdietrich/Documents/VS/UDESA/TP2/soccer.png")
 height_red, width_red, height_blue, width_blue, height_green, width_green = height_width(r,g,b)
 
-dot_size = 5 #LO PONE EL USUARIO
-angle = 15,45,0 #here go there
+
 
 #CONSEGUIMOS LAS COORDENADAS DONDE ESTAN LOS PUNTOS CENTRALES PARA DIBUJAR EL CIRCULO
-chords_list_red = get_grid_coords(height_red ,width_red ,dot_size ,angle)
-chords_list_blue = get_grid_coords(height_blue ,width_blue ,dot_size ,angle)
-chords_list_green = get_grid_coords(height_green ,width_green ,dot_size ,angle)
+chords_list_red = get_grid_coords(height_red ,width_red ,dot_size ,angle_r)
+chords_list_blue = get_grid_coords(height_blue ,width_blue ,dot_size ,angle_g)
+chords_list_green = get_grid_coords(height_green ,width_green ,dot_size ,angle_b)
 
-
-array_white_r = np.full((height_red, width_red),255, dtype = np.uint8)  #Fondo blanco
+#Fondo blanco
+array_white_r = np.full((height_red, width_red),255, dtype = np.uint8)  
 array_white_g = np.full((height_green, width_green),255, dtype = np.uint8)
 array_white_b = np.full((height_blue, width_blue),255, dtype = np.uint8)
 
-for chord in chords_list_red:
-    column = int(chord[0])
-    row = int(chord[1])
+#Dibujo el circulo para cada color
+new_array_white_r = draw_circle(chords_list_red, dot_size, height_red, width_red, array_white_r, r)
+new_array_white_g = draw_circle(chords_list_green, dot_size, height_green, width_green, array_white_g, g)
+new_array_white_b = draw_circle(chords_list_blue, dot_size, height_blue, width_blue, array_white_b, b)
 
-    pixel_intensity = r[row,column]
-
-    max_radius = dot_size/2
-    radius = int(max_radius * (1 - pixel_intensity / 255))
-
-    #RECORRO UNA CUADRICULA CERCA DEL CIRCULO QUE SE FIJE QUE PUNTOS ESTAN DENTRO DEL CIRCULO
-    for i in range((row - radius),(row + radius)+1):  
-        for j in range((column - radius),(column + radius)+1):
-            if 0 <= i < height_red and 0 <= j < width_red: #SE FIJA SI ESTAMOS EN LA MATRIZ
-                if (i - row)**2 + (j - column)**2 <= radius**2: #CHEQUEO ESTO DE ECUACION DADA
-                    array_white_r[i, j] = 0 
-
-for chord in chords_list_green:
-    column = int(chord[0])
-    row = int(chord[1])
-
-    pixel_intensity = g[row,column]
-
-    max_radius = dot_size/2
-    radius = int(max_radius * (1 - pixel_intensity / 255))
-
-    #RECORRO UNA CUADRICULA CERCA DEL CIRCULO QUE SE FIJE QUE PUNTOS ESTAN DENTRO DEL CIRCULO
-    for i in range((row - radius),(row + radius)+1):  
-        for j in range((column - radius),(column + radius)+1):
-            if 0 <= i < height_green and 0 <= j < width_green: #SE FIJA SI ESTAMOS EN LA MATRIZ
-                if (i - row)**2 + (j - column)**2 <= radius**2: #CHEQUEO ESTO DE ECUACION DADA
-                    array_white_g[i, j] = 0 
-
-for chord in chords_list_blue:
-    column = int(chord[0])
-    row = int(chord[1])
-
-    pixel_intensity = b[row,column]
-
-    max_radius = dot_size/2
-    radius = int(max_radius * (1 - pixel_intensity / 255))
-
-    #RECORRO UNA CUADRICULA CERCA DEL CIRCULO QUE SE FIJE QUE PUNTOS ESTAN DENTRO DEL CIRCULO
-    for i in range((row - radius),(row + radius)+1):  
-        for j in range((column - radius),(column + radius)+1):
-            if 0 <= i < height_blue and 0 <= j < width_blue: #SE FIJA SI ESTAMOS EN LA MATRIZ
-                if (i - row)**2 + (j - column)**2 <= radius**2: #CHEQUEO ESTO DE ECUACION DADA
-                    array_white_b[i, j] = 0 
-print("Forma R:", array_white_r.shape)
-print("Forma G:", array_white_g.shape)
-print("Forma B:", array_white_b.shape)
-final_array = np.stack([array_white_r, array_white_g , array_white_b], axis=2)
+#Se unen los tres canales y se guarda la imagen
+final_array = np.stack([new_array_white_r, new_array_white_gn , new_array_white_bn], axis=2)
 nueva_imagen = Image.fromarray(final_array.astype(np.uint8))
-nueva_imagen.save("imagen_invertida4.png")
-print("Forma final_array:", final_array.shape)
+nueva_imagen.save("imagen_invertida7.png")
