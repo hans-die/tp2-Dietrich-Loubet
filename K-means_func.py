534b8dbf041c3e67d5e3977675e2d24592ef5b2c
Francisco Loubet <floubet@udesa.edu.ar>
2025-05-12 20:00:25 -0300

    Update K-means_func.py

--- K-means_func.py
+++ K-means_func.py
@@ -2,9 +2,12 @@ from PIL import Image
 import numpy as np
 import random
 import math
+import time
 
+
+start_time = time.time()
 #PASO 1
-image=Image.open("C:\\Users\\aula\\Documents\\TP2 FRANCISCO LOUBET\\istockphoto-517188688-612x612.jpg").convert("RGB")
+image=Image.open("C:\\Users\\aula\\Documents\\TP2 LOUBET\\images.jpg").convert("RGB")
 pixels=np.array(image)
 
 pixelsList=[]
@@ -25,19 +28,73 @@ centroids=random.sample(pixelsList,clusters_quantity) #DEVUELVE EN UNA LISTA LA 
 
 #PASO 2
 
-for color in pixelsList:
-    R1,G1,B1=color
-    best_distance=10000
+for i in range(100):
     list_pixelsToCentroids=[]
+    for color in pixelsList:
+        R1,G1,B1=map(int,color) #Convierto a int para que el unit no tenga problemas, que en vez de 16 me de -240 cuando hace 10-250
+        best_distance=10000
 
-    for centroid in centroids:
-        R2,G2,B2=centroid
-        #Calculo distancia entre colores
-        distance=math.sqrt((R1-R2)**2 + (G1-G2)**2 + (B1-B2)**2)
+        for h,centroid in enumerate(centroids):
+            R2,G2,B2=map(int,centroid)
+            #Calculo distancia entre colores
+            distance=math.sqrt((R1-R2)**2 + (G1-G2)**2 + (B1-B2)**2)
 
-        if(distance<best_distance):
-            best_distance=distance
-            best_centroid=centroid
+            if(distance<best_distance):
+                best_distance=distance
+                best_index=h            #Guardo el indice del centroide, no el color
 
-        list_pixelsToCentroids.append(best_centroid) #Esta lista son todos los pixeles ya con el centroide asignado y que color van a representar
+        list_pixelsToCentroids.append(best_index) #Esta lista son todos los pixeles ya con el centroide asignado y que color van a representar
 
+    #Paso 3
+    sum_r = {}
+    sum_g = {}
+    sum_b = {}
+    quantity = {}
+
+    for i in range(clusters_quantity): #Hago una estructura para acumular valores RGB y la cantidad de pixeles asignados a cada cluster
+        sum_r[i] = 0
+        sum_g[i] = 0
+        sum_b[i] = 0
+        quantity[i] = 0
+
+    for m in range(len(pixelsList)):
+        pixel = pixelsList[m]
+        index = list_pixelsToCentroids[m]
+        # Busco en los pixeles a que centroice fue asignado y sumo
+
+        r, g, b = pixel
+        sum_r[index] += r
+        sum_g[index] += g
+        sum_b[index] += b
+        quantity[index] += 1
+
+    new_centroids=[]
+
+    for j in range(clusters_quantity):
+        if quantity[j]==0:
+            new_centroids.append(random.choice(pixelsList))
+        else:
+            new_r=round(sum_r[j]/quantity[j])
+            new_g=round(sum_r[j]/quantity[j])
+            new_b=round(sum_r[j]/quantity[j])
+            new_centroids.append([new_r,new_g,new_b])
+
+    if np.allclose(centroids, new_centroids, atol=1): #Corta el bucle si no hay cambios significativos entre los centroides
+        break
+    
+
+#PASO 5 
+
+for row in range (pixels_height):
+    for column in range (pixels_width):# Voy pixel a pixel y reemplazo por el color del centroide final
+        index=row*pixels_width+column
+        centroid_index=list_pixelsToCentroids[index]
+
+        pixels[row][column]=centroids[centroid_index]
+
+Image.fromarray(pixels.astype(np.uint8)).save("imagen_resultado.png")
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Tiempo de ejecución: {elapsed_time:.2f} segundos")
